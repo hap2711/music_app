f7ac595bc5fdbe8bf3346ad975df2857efbe0be9
Aadithya Pai <hap2711@gmail.com>
2018-01-15 16:14:05 -0800

    views updated

--- hap_music/views.py
+++ hap_music/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from . import models
@@ -6,16 +7,26 @@ import json
 
 # Create your views here.
 def index(request):
+    song_list = models.Song.objects.filter(song_name="alchemist")
+    print(song_list)
+    context = {
+        'song_list' : song_list,
+    }
+    #return render(request, 'hap_music/index.html', context)
     return HttpResponse("Welcome to my music app")
 
 @csrf_exempt
 def ret_song_on_genre(request, genre):
     result = []
-    return(models.Song.objects.filter(genre=genre))
+    print(genre)
+    return HttpResponse(models.Song.objects.filter(genre=genre))
+    genre_based_song_list = models.Song.objects.filter(genre=genre)
+    template = loader.get_template('hap_music/genre.html')
+    context = {
+        'genre_based_song_list' : genre_based_song_list,
+    }
 
-    for song in songs:
-        result.append(song.song_name)
-    return HttpResponse(json.dumps(result))
+    return HttpResponse(template.render(context, ))
 
 @csrf_exempt
 def ret_song_on_artist(request, artist):
@@ -24,7 +35,7 @@ def ret_song_on_artist(request, artist):
 @csrf_exempt
 def ret_song_on_album(request, album):
     result = []
-    songs = models.Song.objects.filter(album=album)
+    songs = models.Song.objects.filter(album_title=album)
 
     for song in songs:
         result.append(song.song_name)
